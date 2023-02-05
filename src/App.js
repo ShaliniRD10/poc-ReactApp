eecf71eed468f1e8498cb7ca80043f6261bbe754
spurohit43@gatech.edu <spurohit43@gatech.edu>
2023-02-05 14:41:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React, {useState} from "react"; 
 import './App.css';
+import { AdminPortal } from "./AdminPortal";
+import { UserLogin } from './UserLogin';
+import { AdminLogin } from "./AdminLogin";
+//import {CreateLogin} from "./CreateLogin";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AdminPortal/>
   );
 }
 
